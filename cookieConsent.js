fbbb9f8db1e21820613209fdf4fb25b3617940a4
byDsh116 <by.dsh116@gmail.com>
2023-01-11 12:10:18 +0300

    change cookie storage from function to class

--- cookieConsent.js
+++ cookieConsent.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    class CookieConsent {
+        constructor({ popup, btnConfirm, btnCancel, activeClass = '' } = {}) {
+            this.popup = document.querySelector(popup)
+            this.btnConfirm = document.querySelector(btnConfirm)
+            this.btnCancel = document.querySelector(btnCancel)
+            this.activeClass = activeClass
+            this.consentPropertyType = 'site_consent'
+        }
 
-    const cookieStorage = {
-        getItem: (key) => {
+        getItem = (key) => {
             const cookies = document.cookie
                 .split(';')
                 .map(cookie => cookie.split('='))
@@ -11,36 +18,61 @@ document.addEventListener('DOMContentLoaded', () => {
                 }), {})
 
             return cookies[key]
-        },
-        setItem: (key, value) => {
+        }
+
+        setItem = (key, value) => {
             document.cookie = `${key}=${value};expires=Sun, 10 Jul 2023 18:00:00 GMT`
         }
-    };
 
-    const storageType = cookieStorage
-    const consentPropertyType = 'site_consent'
+        hasConsented = () => {
+            if (this.getItem(this.consentPropertyType) === 'true') {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-    const hasConsented = () => storageType.getItem(consentPropertyType) === 'true' ? true : false
-    const toggleStorage = (prop) => storageType.setItem(consentPropertyType, prop)
+        changeStatus = (prop) => {
+            this.setItem(this.consentPropertyType, prop)
+            if (this.hasConsented()) {
+                myScripts()
+            }
+        }
 
-    const popup = document.querySelector('.popup'),
-        btnConfirm = document.querySelector('[data-confirm]'),
-        btnCancel = document.querySelector('[data-cancel]');
+        /// ниже упростить код
+        bindTriggers = () => {
+            this.btnConfirm.addEventListener('click', () => {
+                this.changeStatus(true)
+                this.popup.classList.remove(this.activeClass)
+            })
 
+            this.btnCancel.addEventListener('click', () => {
+                this.changeStatus(false)
+                this.popup.classList.remove(this.activeClass)
+            })
+        }
+        init = () => {
+            try {
+                if (this.hasConsented()) {
+                    myScripts()
+                } else {
+                    this.popup.classList.add(this.activeClass)
+                }
 
-    if (hasConsented()) {
-        console.log('Loading...')
-    } else {
-        popup.classList.add('popup_active')
+                this.bindTriggers()
+            } catch (e) {
+                console.error('Переданы не все данные')
+            }
+        }
     }
 
-    btnConfirm.addEventListener('click', () => {
-        toggleStorage(true)
-        popup.classList.remove('popup_active')
+    new CookieConsent({
+        activeClass: 'popup_active',
+        popup: '.popup',
+        btnConfirm: '[data-confirm]',
+        btnCancel: '[data-cancel]'
+    }).init()
+    function myScripts() {
         console.log('Loading...')
-    })
-    btnCancel.addEventListener('click', () => {
-        toggleStorage(false)
-        popup.classList.remove('popup_active')
-    })
+    }
 })
